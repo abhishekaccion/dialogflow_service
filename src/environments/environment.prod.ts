814063895c9599a416d471b9632c4ec2c7f18117
Nikhil Pujari <nikhil.pujari@accionlabs.com>
2018-02-28 12:05:24 +0000

    fix(domain url issue)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   intentApiPath: "http://aicstage.accionlabs.com/api/accion",
-  graphqlChatBotPath: "http://localhost:3002/graphql"
+  graphqlChatBotPath: "http://aicstage.accionlabs.com/chatservice/graphql"
 };
